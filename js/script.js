3a2ae296be206b9878b7880d73f6a7530c55a2ff
Krisna Fathurahman <sunkenpotenkim@gmail.com>
2014-06-17 19:05:09 +0700

    resolve big "mess"

--- js/script.js
+++ js/script.js
@@ -1,25 +1,59 @@
-$(document).ready(function()
-{
-	var i = 1;
-	var row, col;
-	
-	while (i < 5)
-	{
-		row = Math.floor((Math.random() * 6) + 2);
-		col = Math.floor((Math.random() * 5) + 1);
-		
-		if( $(".content table tr:nth-child("+ row +") td:nth-child("+ col +")").hasClass("pink") ) 
-		{
+var table = document.getElementById("gameBoard");
+var currPos = [0,1];
 
-		}
-		else
-		{
-			$(".content table tr:nth-child("+ row +") td:nth-child("+ col +")").addClass("pink");
-			i++;
-		}
-	}
+$(document).ready(function(){
+   var i = 1;
+   var row, col;
+   
+   while (i < 5)
+   {
+      row = Math.floor((Math.random() * 6) + 2);
+      col = Math.floor((Math.random() * 5) + 1);
+      
+      if( $(".content table tr:nth-child("+ row +") td:nth-child("+ col +")").hasClass("pink") ) 
+      {
 
-	// $('td.cell').click(function(){
-		// $(this).append('<img src="images/hor-connector1.png" alt="">');
-	// });
+      }
+      else
+      {
+         $(".content table tr:nth-child("+ row +") td:nth-child("+ col +")").addClass("pink");
+         i++;
+      }
+   }
+
+      for (var i = 2; i <= 7; i++) {
+            for (var j = 1; j <= 6; j++) {
+               $('table tr:nth-child('+ i +') td:nth-child('+ j +').cell').click(function(){
+                  if(currPos[0]+1 == this.cellIndex) {
+                     var x = currPos[0]+1;
+                     var y = currPos[1]+1;
+                     $(this).toggleClass("left");
+                     $('table tr:nth-child('+ y +') td:nth-child('+ x +').cell').toggleClass("right");
+                     currPos[0] = this.cellIndex;
+                     currPos[1] = this.parentNode.rowIndex;
+                  } else if(currPos[0]-1 == this.cellIndex) {
+                     var x = currPos[0]+1;
+                     var y = currPos[1]+1;
+                     $(this).toggleClass("right");
+                     $('table tr:nth-child('+ y +') td:nth-child('+ x +').cell').toggleClass("left");
+                     currPos[0] = this.cellIndex;
+                     currPos[1] = this.parentNode.rowIndex;
+                  } else if(currPos[1]+1 == this.parentNode.rowIndex) {
+                     var x = currPos[0]+1;
+                     var y = currPos[1]+1;
+                     $(this).toggleClass("up");
+                     $('table tr:nth-child('+ y +') td:nth-child('+ x +').cell').toggleClass("down");
+                     currPos[0] = this.cellIndex;
+                     currPos[1] = this.parentNode.rowIndex;
+                  } else if(currPos[1]-1 == this.parentNode.rowIndex) {
+                     var x = currPos[0]+1;
+                     var y = currPos[1]+1;
+                     $(this).toggleClass("down");
+                     $('table tr:nth-child('+ y +') td:nth-child('+ x +').cell').toggleClass("up");
+                     currPos[0] = this.cellIndex;
+                     currPos[1] = this.parentNode.rowIndex;
+                  }
+               });
+            }  
+      }
 });
