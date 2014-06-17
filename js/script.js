4a37f1b37f16ead57946c40acd9a9baddbd734b6
Wishnu Ehrgeiz <cloud_strife1310@yahoo.com>
2014-06-17 18:48:15 +0700

    Random kelar

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-$(document).ready(function(){
-	$('td.cell').click(function(){
-		$(this).append('<img src="images/hor-connector1.png" alt="">');
-	});
+$(document).ready(function()
+{
+	var i = 1;
+	var row, col;
+	
+	while (i < 5)
+	{
+		row = Math.floor((Math.random() * 6) + 2);
+		col = Math.floor((Math.random() * 5) + 1);
+		
+		if( $(".content table tr:nth-child("+ row +") td:nth-child("+ col +")").hasClass("pink") ) 
+		{
+
+		}
+		else
+		{
+			$(".content table tr:nth-child("+ row +") td:nth-child("+ col +")").addClass("pink");
+			i++;
+		}
+	}
+
+	// $('td.cell').click(function(){
+		// $(this).append('<img src="images/hor-connector1.png" alt="">');
+	// });
 });
