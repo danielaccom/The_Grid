1e67f988cfc98b40a32ffa40235ae8c1f6443232
13511068 <13511068@std.stei.itb.ac.id>
2014-06-18 09:45:31 +0700

    game logic part 2

--- js/game.js
+++ js/game.js
@@ -6,32 +6,73 @@ function initGame(){
 	
 	//INIT POSITIONS
 	currPos = [0,1]
+	arrTargetPos = {}
 	
-	//INIT 
+	//INIT CONNECTOR COUNT
+	connectorCount = 0;
+	
+	//RANDOMIZE ARRAY POSITION
+	randomizeTargetPos();
+}
+
+	//NEED TO FILL//
+//TARGET RANDOMIZER
+function randomizeTargetPos(){
+	//DUMMY RANDOMIZER
+	arrTargetPos[0] = [1,3]
+	arrTargetPos[1] = [3,3]
+	arrTargetPos[2] = [1,5]
+	arrTargetPos[3] = [3,6]
+	arrTargetPos[4] = [5,6]
+	//DUMMY RANDOMIZER END
 }
 
 //MOVE TO
 function move(newX,newY){
 	
-	//IMPORTANT
-	//NEEDS TO ADD VALIDATION
-	if(true){
 		stackMove.push(currPos);
 		stackCount++;
 		
 		currPos = [newX,newY];
-		alert("moved");
-	}else{
-		alert("Invalid Move");
-	}
+
+}
+
+	//NEED TO FILL//
+//CHECK MOVE VALIDITY, RETURN BOOLEAN
+function isMoveValid(x,y){
+	//DUMMY VALIDATION
+	return true;
+	//END DUMMY VALIDATION
+}
+	
+	//NEED TO FILL
+//CHECK WIN CONDITION
+function isWin(){
+	//DUMMY VALIDATION
+	return true;
+	//END DUMMY VALIDATION
+}
+	
+	//NEED TO FILL
+//CHECK IF CAN UNDO
+function isCanUndo(x,y){
+	//DUMMY VALIDATION
+	return true;
+	//END DUMMY VALIDATION
+}
+
+//FUNCTION FOR UNDO MOVE
+function undoLastMove(){
+	currPos = stackMove.pop();
+	stackCount--;
 }
 
 //VARIABLE DECLARATION
 var stackMove;//STACK FOR UNDO
 var stackCount;
 
-var currPos;////CURRENT POSITION IN X,Y
-var arrTargetPos;//ARRAY POSITION TARGET
+var currPos;////CURRENT POSITION IN X,Y STARTS AT 0
+var arrTargetPos;//ARRAY POSITION TARGET IN X,Y STARTS AT 0
 
 var connectorCount;//CONNECTOR USED
 //BODY
